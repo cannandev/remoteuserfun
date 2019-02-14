5d1cdf5fcb786ef1d87a52582a9598cf6658838d
Claire Annan <claire.annan.me@gmail.com>
2019-02-14 17:55:44 -0500

    Update toggler fu. Add addClass and appender fus.

--- app.js
+++ app.js
@@ -6,13 +6,21 @@ const url = 'https://randomuser.me/api/?results=4&?nat=us,dk,fr,gb' // get 4 ran
 
  // Function to toggle expandables
  const toggler = e => {
-    if (e.target.closest('.accordion__drawer')) {
-      e.target.closest('.accordion__drawer').classList.toggle('is-open')
+    if (e.target.matches('.accordion__profile')) {
+      e.target.parentElement.classList.toggle('is-open')
+      // but now clicking on child elements (button, img, name, etc.) won't open .accordion__contact
     }
-    // currentTarget? I need drawer, not profile
 }
 
- // Function to add a CSS class and append to another element (two params)
+ // Function to add a CSS class 
+ const addClass = (elem, className) => {
+  elem.classList.add(className)
+ }
+
+ // Function to append multiple elements to a parent
+ const appender = (parent, elemArray) => {
+   elemArray.forEach(elem => parent.appendChild(elem))
+ }
 
 fetch(url)
   .then((response) => response.json()) // Transform the data into json
@@ -32,64 +40,59 @@ fetch(url)
           emailInfo = document.createElement('div')
 
       // Get the profile image 
+      addClass(img, 'profile__image');
       img.src = profile.picture.medium
-      img.classList.add('profile__image')
       
       // Get the profile name
-      name.classList.add('profile__name')
+      addClass(name, 'profile__name')
       name.innerHTML = `${profile.name.first} ${profile.name.last}`
       
       // Convert the dob to a date object. Get only the year.
       let year = new Date(profile.dob.date)
-      date.classList.add('profile__date')
+      addClass(date, 'profile__date')
       date.innerHTML = `(${year.getFullYear()})`
       
       // Get the location. Need function to map two-letter country to full name
-      location.classList.add('profile__location')
+      addClass(location, 'profile__location')
       location.innerHTML = `${profile.location.city}, ${profile.nat}` 
 
       // Add all profileInfo content (h2)
-      profileInfo.classList.add('profile__info')
-      profileInfo.appendChild(name)
-      profileInfo.appendChild(date)
-      profileInfo.appendChild(location)
+      addClass(profileInfo, 'profile__info')
+      appender(profileInfo, [name, date, location])
 
-      // Clone the existing element. Need to remove hidden markup.
+      // Clone the existing element. 
+      // @TODO: Need to remove hidden markup.
       let button = document.querySelector('.accordion__indicator.is-hidden')
       let indicator = button.cloneNode(true)
       indicator.classList.remove('is-hidden')
 
       // Add all profile content
-      profileContainer.classList.add('accordion__profile')
-      profileContainer.appendChild(img)
-      profileContainer.appendChild(profileInfo)
-      profileContainer.appendChild(indicator)
+      addClass(profileContainer, 'accordion__profile')
+      appender(profileContainer, [img, profileInfo, indicator])
 
       // Add all contact content
-      phoneInfo.classList.add('contact__phone')
+      addClass(phoneInfo, 'contact__phone')
       phoneInfo.innerHTML = `<h3>Phone</h3><span class="contact__phone-number">${profile.phone}</span>`
-      emailInfo.classList.add('contact__email')
+      
+      addClass(emailInfo, 'contact__email')
       emailInfo.innerHTML = `<h3>Email</h3><span class="contact__email-address">${profile.email}</span>`      
-      contactContainer.classList.add('accordion__contact')
-      contactContainer.appendChild(phoneInfo)
-      contactContainer.appendChild(emailInfo)
+      
+      addClass(contactContainer, 'accordion__contact')
+      appender(contactContainer, [phoneInfo, emailInfo])
 
       // Add content to accordion drawer
-      drawer.classList.add('accordion__drawer')
-      drawer.appendChild(profileContainer)
-      drawer.appendChild(contactContainer)
+      addClass(drawer, 'accordion__drawer')
+      appender(drawer, [profileContainer, contactContainer])
 
       // Finally, attach drawer to accordion
-      accordion.appendChild(drawer)
+      appender(accordion, [drawer])
       
     })
   })
   .then(function(res) {
     // Toggle each accordion. Give large click area, not just arrows.
-    const expandables = accordion.querySelectorAll('.accordion__profile')
-    expandables.forEach(elem => {
-      elem.addEventListener('click', toggler)
-    })
+    // Use event delegation to add listener to parent (.accordion), listen for clicked child (.accordion__profile).
+    accordion.addEventListener('click', toggler)
   })
   .catch(function(error) {
     // Show any data errors
